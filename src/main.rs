d63abf2be02d08a84469def32dd2455718ce3935
ashraf840 <shift2unleashed2011@gmail.com>
2023-07-01 23:17:10 +0600

    Variation of getting value of specific key form a HashMap

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ fn main() {
     // Create a new empty HashMap
     let mut heroes: HashMap<&str, &str> = HashMap::new();
     // heroes.insert("Supermap", "Clark Kent");    // [Question is "Q/A" section]
-    &mut heroes.insert("Supermap", "Clark Kent");
+    &mut heroes.insert("Superman", "Clark Kent");
     &mut heroes.insert("Batman", "Bruce Wayne");
     &mut heroes.insert("The Flash", "Barry Allen");
 
@@ -43,7 +43,13 @@ fn main() {
     println!("Length of \"Heroes\": {}", heroes.len());
 
     // get the value of a specific key from a hashmap
-
+    // VARIATION-1: get the value of key "Batman" / similar to Pythonic way
+    println!("Hero of \"Batman\": {}", heroes["Batman"]);
+    
+    // VARIATION-1: get the value of key "The Flash" / uisng the rust's HashMap's "get()" func
+    let hero_name_option_value: Option<&&str> = heroes.get("The Flash");
+    let hero_name_string_value: String = hero_name_option_value.unwrap().to_string();
+    println!("Hero of \"The Flash\": {}", hero_name_string_value);
 
     // Check if a specific key exists in a hashmap before doing further computation
 
